48965eceb7b8e718666b7a39cff3a849bfa9e333
achyutjagini <achyut.jagini@gmail.com>
2025-12-29 12:31:55 +0100

    changed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ export default function About() {
         What drives me is the process of turning complex data into actionable insights. I love exploring patterns, optimizing systems, and building solutions that make workflows smarter and more efficient. Software engineering and data analysis excite me because they combine creativity, logic, and impact. I get energized by identifying problems, designing solutions, and seeing them come to life.
       </p>
       <p>
-        I enjoy tinkering with model fine-tuning and cloud deployments. I am a big open-source enthusiast—you'll find me contributing to projects that make AI more accessible to everyone.
+        I enjoy tinkering with model fine-tuning and cloud deployments. I am a big open-source enthusiast and you'll find me contributing to projects that make AI more accessible to everyone.
       </p>
       <p>
         In the next few years, I aim to specialize in AI-driven solutions for real-world challenges. I thrive in collaborative environments that value curiosity, innovation, and continuous learning.
